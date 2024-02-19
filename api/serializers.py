129e9f259186367bb503d0394dfac0562c65d79b
Sobhan Khoshdast <s.kht@outlook.com>
2024-02-19 22:54:06 +0100

    Refactoring: Edited JSON API Keys

--- api/serializers.py
+++ api/serializers.py
@@ -2,17 +2,17 @@ from rest_framework import serializers
 from django.utils.translation import gettext_lazy as _
 
 class WeatherSerializer(serializers.Serializer):
-    city_name = serializers.CharField(max_length=255, source='name', label=_('City Name'))
-    temp_current = serializers.FloatField(source='main.temp', label=_("Current Temperature"))
-    temp_min = serializers.FloatField(source='main.temp_min', label=_("Minimum Temperature"))
-    temp_max = serializers.FloatField(source='main.temp_max', label=_("Maximum Temperature"))
+    cityName = serializers.CharField(max_length=255, source='name', label=_('City Name'))
+    tempCurrent = serializers.FloatField(source='main.temp', label=_("Current Temperature"))
+    tempMin = serializers.FloatField(source='main.temp_min', label=_("Minimum Temperature"))
+    tempMax = serializers.FloatField(source='main.temp_max', label=_("Maximum Temperature"))
     humidity = serializers.IntegerField(source='main.humidity', label=_("Humidity"))
     pressure = serializers.IntegerField(source='main.pressure', label=_('Pressure'))
-    wind_speed = serializers.FloatField(source='wind.speed', label=_("Wind Speed"))
-    wind_direction = serializers.SerializerMethodField(method_name='get_wind_direction', label=_("Wind Direction"))
+    windSpeed = serializers.FloatField(source='wind.speed', label=_("Wind Speed"))
+    windDirection = serializers.SerializerMethodField(method_name='get_wind_direction', label=_("Wind Direction"))
     description = serializers.SerializerMethodField(method_name='get_weather_description', label=_("Descriptin"))
 
-    def get_wind_direction(self, obj):
+    def get_wind_direction(self, obj) -> str:
         directions = [
             _('North'),
             _('Northeast'),
@@ -27,9 +27,20 @@ class WeatherSerializer(serializers.Serializer):
         index = int((obj['wind']['deg'] % 360.0) / 45.0)
         return directions[index]
     
-    def get_weather_description(self, obj):
+    def get_weather_description(self, obj) -> str:
         weather_data = obj.get('weather')[0]
         try:
             return weather_data.get('description', 'N/A')
         except Exception as e:
             raise e
+
+class MissingOrIvalidCityErrorSerializer(serializers.Serializer):
+    statusCode = serializers.IntegerField()
+    message = serializers.CharField(max_length=255)
+class CityNotFoundErrorSerializer(serializers.Serializer):
+    statusCode = serializers.IntegerField()
+    message = serializers.CharField(max_length=255)
+
+class InternalServerErrorSerializer(serializers.Serializer):
+    statusCode = serializers.IntegerField()
+    message = serializers.CharField(max_length=255)
