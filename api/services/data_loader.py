0a3c572fc6f572a22d57cc5dddd6c0dc27fd5147
Sobhan Khoshdast <s.kht@outlook.com>
2024-02-19 20:31:40 +0100

    Implemented config.yaml file Validation

--- api/services/data_loader.py
+++ api/services/data_loader.py
@@ -1,6 +1,21 @@
 import yaml
 from django.conf import settings
 
+def validate_config(config_file):
+    valid_langs = ["en", "de", "fr"]
+    valid_cache_times = [5, 10, 60]
+
+    with open(config_file, 'r') as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+
+    if config['lang'] not in valid_langs:
+        raise ValueError("Invalid language specified in configuration")
+
+    if config['cache_time'] not in valid_cache_times:
+        raise ValueError("Invalid cache time specified in configuration")
+
+    return config
+
 class DataLoaderSingleton:
     _instance = None
 
@@ -12,8 +27,7 @@ class DataLoaderSingleton:
         return cls._instance
     
     def load_data(self):
-        with open(settings.API_CONFIG_FILE, 'r') as f:
-            self._config = yaml.load(f, Loader=yaml.FullLoader)
+            self._config = validate_config(settings.API_CONFIG_FILE)
         
     @property
     def config(self):
